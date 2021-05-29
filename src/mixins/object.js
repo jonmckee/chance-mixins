8a08290a4a85e1f78cc573ab3741ae7bfeb46a89
Jon McKee <jmckee@hy-vee.com>
2021-05-29 13:47:07 -0500

    WIP

--- src/mixins/object.js
+++ src/mixins/object.js
@@ -1,1 +1,33 @@
-export default (chance) => () => ({[chance.word()]: chance.word()});
+export default (chance) => (options = {}) => {
+    let valueFunction = () => chance.word();
+
+    if (options.as) {
+        valueFunction = options.as;
+    }
+
+    let object = {
+        [chance.word()]: valueFunction()
+    };
+
+    if (options.withKeys) {
+        const objWithKeys = options.withKeys.reduce((accum, key) => {
+            accum[key] = valueFunction();
+
+            return accum;
+        }, {});
+
+        object = {
+            ...object,
+            ...objWithKeys
+        };
+    }
+
+    if (options.with) {
+        object = {
+            ...object,
+            ...options.with
+        };
+    }
+
+    return object;
+};
--- test/unit/object.spec.js
+++ test/unit/object.spec.js
@@ -1,4 +1,5 @@
 import Chance from 'chance';
+import {jest} from '@jest/globals'
 
 const chance = new Chance();
 
@@ -11,7 +12,76 @@ describe('Chance.object', () => {
 
     test('should be an object', () => {
         expect(typeof object).toStrictEqual('object');
+    });
 
-        expect(Object.keys(object)).toHaveLength(1)
+    test('should have at least one key', () => {
+        expect(Object.keys(object)).toHaveLength(1);
+    });
+
+    describe('when including other data', () => {
+        beforeEach(() => {
+            object = chance.object({
+                with: {
+                    [chance.word()]: chance.word()
+                }
+            });
+        });
+
+        test('should include that data in the object', () => {
+            expect(Object.keys(object)).toHaveLength(2);
+        });
+    });
+
+    describe('when including keys', () => {
+        let withKeys;
+
+        beforeEach(() => {
+            withKeys = chance.n(chance.word, chance.d4());
+
+            object = chance.object({
+                withKeys
+            });
+        });
+
+        test('should include those keys with random values', () => {
+            expect(Object.keys(object)).toStrictEqual(expect.arrayContaining(withKeys));
+        });
+    });
+
+    describe('when value types are dictated', () => {
+        let asFunction,
+            expectedValue;
+
+        beforeEach(() => {
+            expectedValue = chance.natural();
+            asFunction = jest.fn(() => expectedValue);
+
+            object = chance.object({
+                as: asFunction
+            });
+        });
+
+        test('should use provided function', () => {
+            expect(asFunction).toHaveBeenCalledTimes(1);
+        });
+
+        test('should have values of those types', () => {
+            const values = Object.values(object);
+
+            values.forEach((value) => {
+                expect(value).toStrictEqual(expectedValue);
+            });
+        });
+    });
+
+    test('smoke', () => {
+        object = chance.object({
+            withKeys: ['a', 'b', 'c'],
+            as: () => chance.natural(),
+            with: {
+                'd': 0
+            }
+        });
+        
     });
 });
